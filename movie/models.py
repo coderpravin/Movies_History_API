c7c5e022b0bc9ab47eff880efcf0b037455a9754
coderpravin <impravin00@gmail.com>
2025-07-02 15:53:59 +0530

    Add Models in Movie and SAve recored on database

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+movieStatus = (
+    ("Hit", "Hit"),
+    ("Average", "Average"), 
+    ("Flop", "Flop")
+)
+
+class Movie(models.Model):
+    movie_title = models.CharField(max_length=50)
+    movie_actor = models.CharField(max_length=50)
+    movie_release_year = models.DateField()
+    movie_status = models.CharField(max_length=20, choices=movieStatus) 
+
+
+    def __str__(self):
+        return f"The Movie Name :{self.movie_title} and Hero Is : {self.movie_actor}"
+    
+
+    
--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .models import Movie
 # Create your views here.
+
+def movieView(request):
+    movies = list(Movie.objects.all().values())
+    return JsonResponse(movies, safe=False)
